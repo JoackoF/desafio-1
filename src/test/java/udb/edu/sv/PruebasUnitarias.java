a3d505b85bd4d0d84c6df41b14861b67a7eb0bb4
JoackoF <fuentesmo.jaf@gmail.com>
2026-02-26 11:28:27 -0600

    Solving h2 n app compiler issues xd

--- src/test/java/udb/edu/sv/PruebasUnitarias.java
+++ src/test/java/udb/edu/sv/PruebasUnitarias.java
@@ -1,4 +1,47 @@
 package udb.edu.sv;
 
-public class Test {
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+import udb.edu.sv.domain.Profesor;
+import udb.edu.sv.repository.ProfesorRepository;
+
+@SpringBootTest
+public class PruebasUnitarias {
+
+    @Autowired
+    private ProfesorRepository profesorRepo;
+
+    @Test
+    @Transactional
+    @Commit
+    void insertarDatosTest() {
+        // Registro 1
+        Profesor p1 = new Profesor();
+        p1.setNombre("Nuevo Profesor A");
+        profesorRepo.save(p1);
+
+        // Registro 2
+        Profesor p2 = new Profesor();
+        p2.setNombre("Nuevo Profesor B");
+        profesorRepo.save(p2);
+
+        System.out.println("¡Insersión desde Test completada con éxito!");
+    }
+
+    @Test
+    @Transactional
+    void insertarDatosTest2() {
+        Profesor p1 = new Profesor();
+        p1.setNombre("Nuevo Profesor A");
+        profesorRepo.save(p1);
+
+        Profesor p2 = new Profesor();
+        p2.setNombre("Nuevo Profesor B");
+        profesorRepo.save(p2);
+
+        System.out.println("Total profesores: " + profesorRepo.count());
+    }
 }
