67a999b7b3688253195573868472d86d9f863ec5
JoackoF <fuentesmo.jaf@gmail.com>
2026-02-28 22:09:28 -0600

    updating the schema to avoid duplicate errors xd

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,22 +1,22 @@
 -- Tabla Profesor
 CREATE TABLE
-    profesor (
+    IF NOT EXISTS profesor (
         id BIGINT AUTO_INCREMENT PRIMARY KEY,
         nombre VARCHAR(100) NOT NULL
     );
 
 -- Tabla Materia
 CREATE TABLE
-    materia (
+    IF NOT EXISTS materia (
         id BIGINT AUTO_INCREMENT PRIMARY KEY,
         nombre VARCHAR(100) NOT NULL,
         id_profesor BIGINT,
-        FOREIGN KEY (id_profesor) REFERENCES profesor (id)
+        CONSTRAINT fk_materia_profesor FOREIGN KEY (id_profesor) REFERENCES profesor (id)
     );
 
 -- Tabla Alumno
 CREATE TABLE
-    alumno (
+    IF NOT EXISTS alumno (
         id BIGINT AUTO_INCREMENT PRIMARY KEY,
         nombre VARCHAR(100) NOT NULL,
         apellido VARCHAR(100) NOT NULL
@@ -24,10 +24,10 @@ CREATE TABLE
 
 -- Tabla Intermedia (Inscripciones)
 CREATE TABLE
-    alumno_materia (
+    IF NOT EXISTS alumno_materia (
         id_alumno BIGINT,
         id_materia BIGINT,
         PRIMARY KEY (id_alumno, id_materia),
-        FOREIGN KEY (id_alumno) REFERENCES alumno (id),
-        FOREIGN KEY (id_materia) REFERENCES materia (id)
+        CONSTRAINT fk_alumno_materia_alumno FOREIGN KEY (id_alumno) REFERENCES alumno (id),
+        CONSTRAINT fk_alumno_materia_materia FOREIGN KEY (id_materia) REFERENCES materia (id)
     );
