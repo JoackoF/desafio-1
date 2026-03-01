78f051cf9d87e32a1149f98fa8752d08243c4cc9
JoackoF <fuentesmo.jaf@gmail.com>
2026-02-28 21:40:07 -0600

    Fixing properties file beacuse idk java didnt like to work sometimes xd

--- src/test/java/udb/edu/sv/PruebasUnitarias.java
+++ src/test/java/udb/edu/sv/PruebasUnitarias.java
@@ -9,39 +9,21 @@ import udb.edu.sv.domain.Profesor;
 import udb.edu.sv.repository.ProfesorRepository;
 
 @SpringBootTest
+@Transactional
+@Commit
 public class PruebasUnitarias {
 
     @Autowired
     private ProfesorRepository profesorRepo;
 
     @Test
-    @Transactional
-    @Commit
-    void insertarDatosTest() {
-        // Registro 1
+    void insertarProfesoresTest() {
         Profesor p1 = new Profesor();
-        p1.setNombre("Nuevo Profesor A");
-        profesorRepo.save(p1);
-
-        // Registro 2
-        Profesor p2 = new Profesor();
-        p2.setNombre("Nuevo Profesor B");
-        profesorRepo.save(p2);
-
-        System.out.println("¡Insersión desde Test completada con éxito!");
-    }
-
-    @Test
-    @Transactional
-    void insertarDatosTest2() {
-        Profesor p1 = new Profesor();
-        p1.setNombre("Nuevo Profesor A");
+        p1.setNombre("Profesor Test 1");
         profesorRepo.save(p1);
 
         Profesor p2 = new Profesor();
-        p2.setNombre("Nuevo Profesor B");
+        p2.setNombre("Profesor Test 2");
         profesorRepo.save(p2);
-
-        System.out.println("Total profesores: " + profesorRepo.count());
     }
 }
